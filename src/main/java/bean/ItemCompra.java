c8cf58f6d43fb139478560d0831e58d9b8c0ca75
CristianBY <cristianyelmo@gmail.com>
2019-04-04 13:22:33 +0200

    inicial

---
+++ src/main/java/bean/ItemCompra.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class ItemCompra extends Producto{
+	
+	private Double cantidad;
+
+	public ItemCompra() {
+
+	}
+	public ItemCompra(int sku,double precio,String name,double cantidad) {
+		super(sku,name,precio);
+		this.cantidad = cantidad;
+	}
+	public ItemCompra(Producto p, double cantidad) {
+		super(p.getSku(),p,getName(),p.getPrecio());
+		this.cantidad = cantidad;
+	}
+
+	public void setCantidad(double cantidad){
+		this.cantidad = cantidad;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+}
